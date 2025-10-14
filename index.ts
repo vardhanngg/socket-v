6ce414e660ebb3c05ef74bbe938d6b880dc5480f
Mr.V <67038533+vardhanngg@users.noreply.github.com>
2025-10-14 06:51:38 +0000

    log

--- index.ts
+++ index.ts
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { v2 as cloudinary } from "cloudinary";
+console.log("Cloudinary config env vars:", {
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET ? '***' : undefined,
+});
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
